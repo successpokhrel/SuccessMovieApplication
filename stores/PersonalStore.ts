d82e52893d5cac3cdab4736654cd8d64310dacb9
successpokhrel <successpokhrel25@gmail.com>
2024-12-12 11:48:02 +0545

    zustand middleware to persist favorites and watchlist

--- stores/PersonalStore.ts
+++ stores/PersonalStore.ts
@@ -1,54 +1,64 @@
 import { create } from "zustand";
 import { SingleMovieType } from '../types/MovieTypes';
+import { devtools, persist } from "zustand/middleware";
 
-type PersonalStore = {
+interface PersonalStore {
     favorites: SingleMovieType[];
     watchlist: SingleMovieType[];
     addToFavorites: (favoriteMovie: SingleMovieType) => void;
     addToWatchlist: (favoriteMovie: SingleMovieType) => void;
     removeFromFavorites: (movieId: number) => void;
     removeFromWatchlist: (movieId: number) => void;
-};
+}
 
-const usePersonalStore = create<PersonalStore>((set) => ({
-    favorites: [],
-    watchlist: [],
-    addToFavorites: (favoriteMovie: SingleMovieType) => {
-        set((store) => {
-          const isAlreadyFavorite = store.favorites.some((movie) => movie.id === favoriteMovie.id);
-          if (!isAlreadyFavorite) {
-            return {
-              favorites: [...store.favorites, favoriteMovie],
-            };
-          }
-          return store;
-        });
-      },
-    addToWatchlist: (watchListMovie: SingleMovieType) => {
-        set((store) => {
-            const isAlreadyAdded = store.watchlist.some((movie)=>movie.id === watchListMovie.id);
-            if(!isAlreadyAdded){
-                return{
-                    watchlist: [...store.watchlist, watchListMovie]
-                }
+const usePersonalStore = create<PersonalStore>()(
+  devtools(
+    persist(
+      (set) => ({
+        favorites: [],
+        watchlist: [],
+        addToFavorites: (favoriteMovie: SingleMovieType) => {
+          set((store) => {
+            const isAlreadyFavorite = store.favorites.some(
+              (movie) => movie.id === favoriteMovie.id
+            );
+            if (!isAlreadyFavorite) {
+              return {
+                favorites: [...store.favorites, favoriteMovie],
+              };
             }
             return store;
-        });
-    },
-    removeFromFavorites: (movieId: number) =>{
-        set((store)=>{
-            return{
-                favorites: store.favorites.filter((movie)=>movie.id !== movieId),
+          });
+        },
+        addToWatchlist: (watchListMovie: SingleMovieType) => {
+          set((store) => {
+            const isAlreadyAdded = store.watchlist.some(
+              (movie) => movie.id === watchListMovie.id
+            );
+            if (!isAlreadyAdded) {
+              return {
+                watchlist: [...store.watchlist, watchListMovie],
+              };
             }
-        })
-    },
-    removeFromWatchlist: (movieId: number) =>{
-        set((store)=>{
-            return{
-                watchlist: store.watchlist.filter((movie)=>movie.id !== movieId),
-            }
-        })
-    },
-}));
+            return store;
+          });
+        },
+        removeFromFavorites: (movieId: number) => {
+          set((store) => ({
+            favorites: store.favorites.filter((movie) => movie.id !== movieId),
+          }));
+        },
+        removeFromWatchlist: (movieId: number) => {
+          set((store) => ({
+            watchlist: store.watchlist.filter((movie) => movie.id !== movieId),
+          }));
+        },
+      }),
+      {
+        name: 'SuccessStore',
+      }
+    )
+  )
+);
 
 export default usePersonalStore;
